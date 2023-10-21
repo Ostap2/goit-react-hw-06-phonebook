5363ba1b949b08f330eb9ac7e1f57a5d654dd2b5
Ostap <Ostapkriven13@gmail.com>
2023-10-21 14:39:31 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { addContact } from 'redux/contactsSlice';
+import { addContact } from 'redux/contactsSlice'; 
 import ContactForm from 'components/Contact/ContactForm/ContactForm';
 import ContactList from 'components/Contact/ContactItem/ContactList';
 import Filter from 'components/Contact/Filter/Filter';
@@ -9,10 +9,18 @@ export default function App() {
   const { contacts } = useSelector(state => state.contacts);
   const dispatch = useDispatch();
 
+  const formSubmitHandler = contact => {
+    if (contacts.find(({ name }) => name === contact.name)) {
+      return alert(`${contact.name} is already in contacts`);
+    }
+
+    dispatch(addContact(contact));
+  };
+
   return (
     <Container>
       <h1>Phonebook</h1>
-      <ContactForm />
+      <ContactForm onSubmit={formSubmitHandler} />
       <h2>Contacts</h2>
       {contacts.length === 0 ? (
         <h3>No contacts</h3>
